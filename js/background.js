994c7af0436d7c021e5aae95eaea5078a56b3b8a
Arjun <69323324+arjunken@users.noreply.github.com>
2020-09-14 16:53:09 -0700

    Quote of the Day functionality code was re-written and moved to background.js

--- js/background.js
+++ js/background.js
@@ -1,9 +1,81 @@
 
+chrome.runtime.onInstalled.addListener(function() {
+    var today = new Date();
+    chrome.storage.sync.set({'installedDate': today.toDateString(),'runningDate' : today.toDateString(), 'quoteText' : "Genius is one percent inspiration and ninety-nine percent perspiration.", 'quoteAuthor' : "Thomas Edison"}, function() {
+      console.log('Settings saved'); 
+      
+    });
+
+  });
+
+
 chrome.browserAction.onClicked.addListener(function(activeTab)
 {
-    chrome.tabs.create({ url: "index.html" });
+// Making HomeTab as a new tab in the browser
+chrome.tabs.create({ url: "index.html" });
     
 });
+
+
+chrome.tabs.onCreated.addListener(function(tab){ 
+
+    //Save a daily quote to Chrome Storage for content script to pick up
+
+    var today = new Date();
+
+    chrome.storage.sync.get(['installedDate','runningDate'], function(datesObj){
+
+      var idate = new Date(datesObj.installedDate);
+      var rdate = new Date(datesObj.runningDate);
+
+  if(rdate.toDateString() != today.toDateString()){
+    
+    //Functions to read JSON file and to find difference dates
+
+      function readTextFile(file, callback) {
+      var rawFile = new XMLHttpRequest();
+      rawFile.overrideMimeType("application/json");
+      rawFile.open("GET", file, true);
+      rawFile.onreadystatechange = function() {
+          if (rawFile.readyState === 4 && rawFile.status == "200") {
+              callback(rawFile.responseText);          
+          }
+      }
+      rawFile.send(null);
+      }
+
+      //get the index of quote array for the running date
+            
+      var currentQtIndex = Math.floor((Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()) - Date.UTC(idate.getFullYear(), idate.getMonth(), idate.getDate()) ) /(1000 * 60 * 60 * 24));
+
+      //get the quote from Quotes JSON data file
+
+      readTextFile("../data/quotes.json", function(text){
+        var data = JSON.parse(text);
+        var fetchedquote;
+        if (currentQtIndex <= data.length){
+          fetchedquote = [data[currentQtIndex].text, data[currentQtIndex].author];
+          chrome.storage.sync.set({'quoteText' : fetchedquote[0],'quoteAuthor' : fetchedquote[1],'runningDate' : today}, function() {
+          message('Quote of the day updated!'); 
+          });
+            
+        }else{    
+          currentQtIndex = ((currentQtIndex - (data.length * Math.trunc(( currentQtIndex / data.length))))-1);
+          fetchedquote = [data[currentQtIndex].text, data[currentQtIndex].author];
+          chrome.storage.sync.set({'quoteText' : fetchedquote[0],'quoteAuthor' : fetchedquote[1],'runningDate' : today}, function() {
+          message('Quote of the day updated!'); 
+          });
+
+        }        
+      
+
+        });
+  } 
+
+});
+ 
+});
+
 
 
 
--- js/hometab.js
+++ js/hometab.js
@@ -15,51 +15,14 @@ var interval = setInterval(runClock, 1000)
 
 //**QUOTES SCRIPT */
 
-//Functions to read JSON file and to find difference dates
-
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);          
-        }
-    }
-    rawFile.send(null);
-  }
+chrome.storage.sync.get(['quoteText','quoteAuthor'], function(qtObj) {
   
-  var date_diff_indays = function(date1, date2) {
-           var dt1 = new Date(date1);
-           var dt2 = new Date(date2);
-           return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
-  }
+  document.querySelector("#quoteblock").innerHTML = qtObj.quoteText;
+  document.querySelector("#author").innerHTML = qtObj.quoteAuthor;
   
+});
+ 
   
-  //initialize the beginning date
-  var initializedDate = '09/09/2020';
-  var currentQtIndex = date_diff_indays(initializedDate, new Date())
-  
-  //get the quote from Quotes JSON data file
-  
-  readTextFile("./data/quotes.json", function(text){
-    var data = JSON.parse(text);
-    var fetchedquote;
-    if (currentQtIndex <= data.length){
-      fetchedquote = [data[currentQtIndex].text, data[currentQtIndex].author];
-        
-    }else{    
-      fetchedquote = ((currentQtIndex - (data.length * Math.trunc(( currentQtIndex / data.length))))-1);
-    }
-  
-  const QUOTEELEMENT = document.querySelector("#quoteblock");
-  const AUTHORELEMENT = document.querySelector("#author");
-  
-  // place the quote in the html element
-  QUOTEELEMENT.innerHTML = fetchedquote[0];
-  AUTHORELEMENT.innerHTML = fetchedquote[1];
-  
-  });
 
 
 //*** WEATHER SCRIPT */       
