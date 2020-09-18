89ac3650f6eeb0e7b48b494a9bbd79b9aab36e81
Arjun <69323324+arjunken@users.noreply.github.com>
2020-09-17 22:40:52 -0700

    Word of the Day! card has been added. Everyday 10 new words will be rotated and presented to the user every time a new tab is opened.

--- js/background.js
+++ js/background.js
@@ -1,14 +1,58 @@
+//*** FUNCTIONS DECLARATIONS */
+ //Functions to read JSON file and to find difference dates
 
+ function readTextFile(file, callback) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+          callback(rawFile.responseText);          
+      }
+  }
+  rawFile.send(null);
+  }
+
+  var date_diff_indays = function(date1, date2) {
+    var dt1 = new Date(date1);
+    var dt2 = new Date(date2);
+    return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+  }
+
+  
+//****** INITIALIZE DATA WHEN EXTENSION IS INSTALLED */
 chrome.runtime.onInstalled.addListener(function() {
     var today = new Date();
-    chrome.storage.sync.set({'installedDate': today.toDateString(),'runningDate' : today.toDateString(), 'quoteText' : "Genius is one percent inspiration and ninety-nine percent perspiration.", 'quoteAuthor' : "Thomas Edison"}, function() {
-      console.log('Settings saved'); 
-      
+
+    //** Quotes Data */
+    readTextFile("../data/quotes.json", function(text){
+      let data = JSON.parse(text);
+      let index = Math.floor(Math.random() * data.length);
+      chrome.storage.sync.set({'installedDate': today.toDateString(),'runningDate' : today.toDateString(), 'quoteText' : data[index].text, 'quoteAuthor' : data[index].author}, function() {
+      console.log('Initial Quote saved');       
+      });
     });
+
+    //** Words Data */
+    readTextFile("../data/MasterWordList.json", function(text){
+      let data = JSON.parse(text);
+      let index;
+      let tenwords = new Array(10);
+      for(var i=0;i<10;i++){ 
+         index = Math.floor(Math.random() * data.length);
+        tenwords[i] = data[index].FIELD1;
+        } 
+      chrome.storage.sync.set({'todayswords': tenwords}, function() {
+      console.log('Initial words saved');           
+      });
+    });
+
+
+
 
   });
 
-
+//****** CODE TO MAKE HOMETAB AS DEFAULT NEW TAB  */
 chrome.browserAction.onClicked.addListener(function(activeTab)
 {
 // Making HomeTab as a new tab in the browser
@@ -16,6 +60,7 @@ chrome.tabs.create({ url: "index.html" });
     
 });
 
+//****** UPDATE QUOTES DAILY  */
 
 chrome.tabs.onCreated.addListener(function(tab){ 
 
@@ -23,54 +68,38 @@ chrome.tabs.onCreated.addListener(function(tab){
 
     var today = new Date();
 
-    chrome.storage.sync.get(['installedDate','runningDate'], function(datesObj){
+    chrome.storage.sync.get(['installedDate','runningDate','quoteText','quoteAuthor'], function(datesObj){
 
       var idate = new Date(datesObj.installedDate);
       var rdate = new Date(datesObj.runningDate);
-
-  if(rdate.toDateString() != today.toDateString()){
+     
+    if((rdate.toDateString() != today.toDateString()) || datesObj.quoteText==null || datesObj.quoteAuthor==null){
     
-    //Functions to read JSON file and to find difference dates
-
-      function readTextFile(file, callback) {
-      var rawFile = new XMLHttpRequest();
-      rawFile.overrideMimeType("application/json");
-      rawFile.open("GET", file, true);
-      rawFile.onreadystatechange = function() {
-          if (rawFile.readyState === 4 && rawFile.status == "200") {
-              callback(rawFile.responseText);          
-          }
-      }
-      rawFile.send(null);
-      }
-
-      //get the index of quote array for the running date
-            
-      var currentQtIndex = Math.floor((Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()) - Date.UTC(idate.getFullYear(), idate.getMonth(), idate.getDate()) ) /(1000 * 60 * 60 * 24));
-
-      //get the quote from Quotes JSON data file
+      //get the quote from Quotes JSON data file and update storage
 
       readTextFile("../data/quotes.json", function(text){
         var data = JSON.parse(text);
-        var fetchedquote;
-        if (currentQtIndex <= data.length){
-          fetchedquote = [data[currentQtIndex].text, data[currentQtIndex].author];
-          chrome.storage.sync.set({'quoteText' : fetchedquote[0],'quoteAuthor' : fetchedquote[1],'runningDate' : today}, function() {
-          message('Quote of the day updated!'); 
-          });
-            
-        }else{    
-          currentQtIndex = ((currentQtIndex - (data.length * Math.trunc(( currentQtIndex / data.length))))-1);
-          fetchedquote = [data[currentQtIndex].text, data[currentQtIndex].author];
-          chrome.storage.sync.set({'quoteText' : fetchedquote[0],'quoteAuthor' : fetchedquote[1],'runningDate' : today}, function() {
-          message('Quote of the day updated!'); 
-          });
-
-        }        
-      
-
+        var index = Math.floor(Math.random() * data.length);
+        chrome.storage.sync.set({'quoteText' : data[index].quoteText,'quoteAuthor' : data[index].quoteAuthor,'runningDate' : today}, function() {
+        console.log('Quote of the day updated!'); 
+        });       
         });
-  } 
+
+      //get ten new words and update the storage
+
+      var wdIndex = 10* date_diff_indays(idate, new Date());
+      let index;
+      let tenwords = new Array(10);
+      for(var i=0;i<10;i++){ 
+         index = Math.floor(Math.random() * (data.length - wdIndex) ) + wdIndex;         
+         tenwords[i] = data[index].FIELD1;
+        } 
+      chrome.storage.sync.set({'todayswords': tenwords}, function() {
+      console.log('words updated for today!');           
+      });
+
+
+    } 
 
 });
  
--- js/hometab.js
+++ js/hometab.js
@@ -1,3 +1,17 @@
+//**Function Declarations**//
+ function readTextFile(file, callback) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+          callback(rawFile.responseText);          
+      }
+  }
+  rawFile.send(null);
+  }
+
+
 //**Local Clock */
 const CURRENTTIME = document.querySelector("#timedisplay");
 
@@ -13,21 +27,72 @@ CURRENTTIME.innerHTML = date.toDateString() + " " + date.toLocaleTimeString();
 
 var interval = setInterval(runClock, 1000)
 
-//**QUOTES SCRIPT */
+chrome.storage.sync.get(['quoteText','quoteAuthor','todayswords'], function(dataObj) {
+  
+  //**QUOTES SCRIPT */
+  document.querySelector("#quoteblock").innerHTML = dataObj.quoteText;
+  document.querySelector("#author").innerHTML = dataObj.quoteAuthor;
 
-chrome.storage.sync.get(['quoteText','quoteAuthor'], function(qtObj) {
+  //*** WORD OF THE DAY */
+  let index = Math.floor(Math.random() * dataObj.todayswords.length);
+  document.querySelector("#wordoftheday").innerHTML = dataObj.todayswords[index];
   
-  document.querySelector("#quoteblock").innerHTML = qtObj.quoteText;
-  document.querySelector("#author").innerHTML = qtObj.quoteAuthor;
+  //** Call google word definition API for defnitions */
+  let URL = "https://api.dictionaryapi.dev/api/v2/entries/en/" + dataObj.todayswords[index];
+  var mwURL = "https://www.merriam-webster.com/dictionary/" + dataObj.todayswords[index];
+  readTextFile(URL, function(text){
+    var data = JSON.parse(text);
+    if( data.length>0){
+      if(data[0].meanings.length>0){
+        if(data[0].meanings[0].definitions.length>0){
+          document.querySelector("#wod-def").innerHTML = data[0].meanings[0].definitions[0].definition;
+          if(data[0].meanings[0].definitions[0].example!=null){
+          document.querySelector("#wod-ex").innerHTML = data[0].meanings[0].definitions[0].example;
+          }else{
+          document.querySelector("#wod-ex").innerHTML = "No examples found"
+          }
+          if(data[0].meanings[0].definitions[0].synonyms!=null){
+          document.querySelector("#wod-syn").innerHTML = String(data[0].meanings[0].definitions[0].synonyms).trim().replace(/,/g,', ');
+          }else{
+          document.querySelector("#wod-syn").innerHTML = "No synonyms found";
+          }
+        }
+        document.querySelector("#wod-pos").innerHTML = data[0].meanings[0].partOfSpeech;
+      
+       if(data[0].phonetics.length>0){
+          document.querySelector("#wod-phonetics").innerHTML = data[0].phonetics[0].text;
+          /* console.log(data[0].phonetics[0].audio);*/
+        }
+
+          document.querySelector("#wod-link").innerHTML = "Read more about the word";
+          document.querySelector("#wod-link").setAttribute("href",mwURL);
+
+      } else{
+
+          document.querySelector("#wod-link").innerHTML = "Tough one! Check the Dictionary!";
+          document.querySelector("#wod-link").setAttribute("href",mwURL);
+      }        
+
+    } else {
+     
+          document.querySelector("#wod-link").innerHTML = "Tough one! Check the Dictionary!";
+          document.querySelector("#wod-link").setAttribute("href",mwURL);
+
+    } 
+    
+
+  });
+
   
+
+
 });
- 
   
 
 
-//*** WEATHER SCRIPT */       
 
 
-//*** WORD OF THE DAY */
 
+
+//*** WEATHER SCRIPT */  
 
