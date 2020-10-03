79fd7f35e9ca246755346aa90c4d1b1ea477a347
Arjun <69323324+arjunken@users.noreply.github.com>
2020-10-02 17:37:37 -0700

    Built Search Page.

--- js/hometab.js
+++ js/hometab.js
@@ -15,70 +15,105 @@ let month = date.getMonth();
 
 var interval = setInterval(runClock, 1000);
 
-// external js: packery.pkgd.js
+//Navigation Functionality
+$('#wzToggle').css('display','none');
+$(".grid").css('display','none');
+$(".BookmarkPage").css('display','none');
+
+$("#SearchBtn").click(function(){
+  $(".SearchPage").css('display','');
+  $(".grid").css('display','none');
+  $('#wzToggle').css('display','none');
+  $('.BookmarkPage').css('display','none');
+  $("#searchbox").focus();
+})
+$("#WidgetsBtn").click(function(){
+  $(".grid").css('display','');
+  $('#wzToggle').css('display','');
+  $(".SearchPage").css('display','none');
+  $('.BookmarkPage').css('display','none');
+})
+$("#BookmarkBtn").click(function(){
+  $('.BookmarkPage').css('display','');
+  $(".SearchPage").css('display','none');
+  $(".grid").css('display','none');
+  $('#wzToggle').css('display','none'); 
+})
+
+// Functionality for Search Page
+
+$("#searchbox").on('keyup', function (e) {
+  if (e.key === 'Enter' || e.keyCode === 13) {
+    
+    var searchterms = $("#searchbox").val();
+    var searchType = $("#optionsbox").find(":selected").text();
+     
+    if(searchType == "Web Search"){
+     var URL = "https://www.google.com/search?q="+ searchterms;
+     window.open(URL, "_self");
+    }
+      
+  }
+});
+
+
+// packery and draggable initialization
 var $grid = $('.grid').packery({
   itemSelector: '.grid-item', 
   columnWidth: 20,
   initLayout: true
 });
 
-// make all grid-items draggable
+var draggies = [];
+var isDrag = true;
 function makeDraggable(){
-$grid.find('.grid-item').each( function( i, gridItem ) {
-  var draggie = new Draggabilly( gridItem );
-  // bind drag events to Packery
-  $grid.packery( 'bindDraggabillyEvents', draggie );
-});
-};
-makeDraggable();
+  $grid.find('.grid-item').each( function( i, gridItem ) {
+    var draggie = new Draggabilly( gridItem );
+     // bind drag events to Packery
+     draggies.push( draggie );
+    $grid.packery( 'bindDraggabillyEvents', draggie );    
+  });
+  };
 
+// create widgets
 $('#widgetSave').on( 'click', function() {
-  // create new item elements
-  // var $items = getItemElement().add( getItemElement() ).add( getItemElement() );
-
   let n = $(".widget").length+1;
-  
-  // let wname = document.forms["wform"]["wname"].value;
   let wcolor = document.forms["wform"]["wcolor"].value;
   let wwidth = document.forms["wform"]["wwidth"].value;
-
-  // if(wname==""){
-  //   wname = "Widget Title";
-  // }
-
   if(wwidth==1){
     var $items = ('<div id="widget-'+n+'" class="grid-item" style="background-color:'+ wcolor +';"></div>');   
   } else {
     var $items = ('<div id="widget-'+n+'" class="grid-item grid-item--width'+ wwidth +'" style="background-color:'+ wcolor +';"></div>');
   }
-   
-
-
-  // append elements to container
-  $grid.prepend( $items )
-    // add and lay out newly appended elements
+  $grid.prepend( $items )    
   $grid.packery( 'prepended', $items );
   $grid.packery('destroy');
   $grid = $('.grid').packery({
     itemSelector: '.grid-item',
     columnWidth: 20,
     initLayout: true // disable initial layout
-  });
-  makeDraggable();
-  
+   });
+  // makeDraggable();  
 });
 
+//trigger draggable only when Rearrange button is clicked
+ $("#rearrange").click(function(){
+  if(isDrag){
+    $grid.packery('destroy');
+    $grid = $('.grid').packery({
+      itemSelector: '.grid-item',
+      columnWidth: 20,
+      initLayout: true // disable initial layout
+     });     
+    makeDraggable();
+  } else{
+    draggies.forEach( function( draggie ) {
+      draggie[0]();
+    });
+  }
+  // switch flag
+  isDrag = !isDrag;
+});
 
-// make <div class="grid-item grid-item--width# grid-item--height#" />
-// function getItemElement() {
-//   var $item = $('<div class="grid-item"></div>');
-//   // add width and height class
-//   var wRand = Math.random();
-//   var hRand = Math.random();
-//   var widthClass = wRand > 0.85 ? 'grid-item--width3' : wRand > 0.7 ? 'grid-item--width2' : '';
-//   var heightClass = hRand > 0.85 ? 'grid-item--height3' : hRand > 0.5 ? 'grid-item--height2' : '';
-//   $item.addClass( widthClass ).addClass( heightClass );
-//   return $item;
-// }
 
 });
